7395804e673404c592f661bec1a4f749c5534136
Danny Bao <88077806+DannyBoy5930@users.noreply.github.com>
2024-03-21 16:13:54 +1100

    Add hardcoded stats jsx

--- src/Stats.jsx
+++ src/Stats.jsx
@@ -1,3 +1,19 @@
 export default function Stats() {
-  return <section className="stats">Stats</section>;
+  return (
+    <section className="stats">
+      <Stat />
+      <Stat />
+      <Stat />
+      <Stat />
+    </section>
+  );
+}
+
+function Stat() {
+  return (
+    <section className="stat">
+      <span className="stat__number">0</span>
+      <h2 className="second-heading">Words</h2>
+    </section>
+  );
 }
