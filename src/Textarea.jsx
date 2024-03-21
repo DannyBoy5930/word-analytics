9ab34c2f57407117766d943b31f0f246ecea5a00
Danny Bao <88077806+DannyBoy5930@users.noreply.github.com>
2024-03-21 12:51:44 +1100

    Add check and warning for script string in textarea

--- src/Textarea.jsx
+++ src/Textarea.jsx
@@ -1,20 +1,30 @@
 import { useState } from "react";
+import Warning from "./Warning";
 
 export default function Textarea() {
   const [text, setText] = useState("");
+  const [showWarning, setShowWarning] = useState(false);
 
   const handleChange = (e) => {
-    const newText = e.target.value;
+    let newText = e.target.value;
+
+    if (newText.includes("<script>")) {
+      setShowWarning(true);
+      newText = newText.replace("<script>", "");
+    }
+
     setText(newText);
   };
 
   return (
-    <textarea
-      value={text}
-      className="textarea"
-      onChange={handleChange}
-      placeholder="Enter your text"
-      spellCheck="false"
-    />
+    <div className="textarea">
+      <textarea
+        value={text}
+        onChange={handleChange}
+        placeholder="Enter your text"
+        spellCheck="false"
+      />
+      {showWarning ? <Warning /> : null}
+    </div>
   );
 }
