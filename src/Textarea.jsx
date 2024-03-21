40b6c07324ab6ccc98dfe55c605bfe2246306ac1
Danny Bao <88077806+DannyBoy5930@users.noreply.github.com>
2024-03-21 12:40:30 +1100

    Update the textarea so that it is controlled i.e. value driven by React state

--- src/Textarea.jsx
+++ src/Textarea.jsx
@@ -1,11 +1,16 @@
+import { useState } from "react";
+
 export default function Textarea() {
+  const [text, setText] = useState("");
+
   const handleChange = (e) => {
-    console.log("change event...");
-    console.log(e.target.value);
+    const newText = e.target.value;
+    setText(newText);
   };
 
   return (
     <textarea
+      value={text}
       className="textarea"
       onChange={handleChange}
       placeholder="Enter your text"
